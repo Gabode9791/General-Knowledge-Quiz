ca0e6ba98393c2bde2cece37bfa52ca78ead9ef1
gabrielifeadike2 <gabrielifeadike2@gmail.com>
2026-07-20 12:46:56 +0100

    Third Project

--- hello.py
+++ hello.py
@@ -1,18 +1,12 @@
-operator = input("Enter an operator (+ - * /): ")
-num1 = float(input("Enter the 1st number: "))
-num2 = float(input("Enter the 2nd number: "))
-if operator == "+":
-    result = num1 + num2
-    print(result)
-elif operator == "-":
-    result = num1 - num2
-    print(result)
-elif operator == "*":
-    result = num1 * num2
-    print(result)
-elif operator == "/":
-    result = num1 / num2
-    print(result)
-else:
-    print(f"{operator} is not a valid operator ")
+unit = input("Is this temperature in Celsius or Fahrenheit: (C or F): ")
+temp = float(input("Enter the temperature: " ))
+
+if unit == "C":
+    temp = round((9 * temp) / 5 + 32, 1)
+    print(f" The temperature in Fahrenheit is: {temp}°F")
+elif unit == "F":
+    temp = round((temp - 32) * 5 / 9, 1)
+    print(f"The temperature in Celsius is: {temp}°C")
     
+else:
+    print(f"{unit} is an invalid unit of measurement")
