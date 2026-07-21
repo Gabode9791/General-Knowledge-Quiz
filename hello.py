a716f1159d484b0dff612a788b39d11cf75287c3
gabrielifeadike2 <gabrielifeadike2@gmail.com>
2026-07-21 17:33:56 +0100

    Update quiz code

--- hello.py
+++ hello.py
@@ -1,23 +1,48 @@
-foods = []
-prices = []
-total = 0
+questions = ("How many elements are in the periodic table?: ", 
+             "Which animal lays the largest eggs?: ",
+             "What is the most abundant gas in Earth's atmosphere?: ",
+             "How many bones are in the human body?: ", 
+             "Which planet in the solar system is the hottest?: ")
 
-while True:
-    food = input("Enter a food to buy (q to quit): ")
-    if food.lower() == "q":
-        break
+options = (("A. 116", "B. 117", "C. 118", "D. 119"),
+           ("A. Whale", "B. Crocodile", "C. Elephant", "D. Ostrich"), 
+           ("A. Nitrogen", "B. Oxygen", "C. Carbon-Dioxide", "D. Hydrogen"), 
+           ("A. 206", "B. 207", "C. 208", "D. 209"), 
+           ("A. Mercury", "B. Venus", "C. Earth", "D. Mars"))
+answers = ("C", "D", "A", "A", "B")
+guesses = []
+score = 0
+question_num = 0
+     
+for question in questions:
+    print("--------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+     
+    guess = input("Enter (A, B, C , D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT!")
     else:
-        price = float(input(f"Enter the price of a {food}: £"))
-        foods.append(food)
-        prices.append(price)
+        print("INCORRECT!")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
 
-print("----- YOUR CART -----")
+print("--------------------")
+print("      RESULTS       ")
+print("--------------------")
 
-for food in foods:
-    print(food, end=" ")
-
-for price in prices:
-    total += price
-
+print("answers: ", end="")
+for answer in answers:
+    print(answer, end=" ")
 print()
-print(f"Your total is £{total}")
+
+print("guesses: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+print()
+score = int(score / len(questions)* 100)
+print(f"You got: {score}%")
+print("Well done lil bro")
